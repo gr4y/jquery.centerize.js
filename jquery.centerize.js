e893a108550a645dd8550f40fd6679cc375794ac
Sascha Wessel <swessel@gr4yweb.de>
2012-01-18 10:11:21 +0100

    Erm. When calculating the left position, $(window).scrollTop(); is a little bit wrong. But why did it worked yesterday?! Anyways. Changed it. Now it works again.

--- jquery.centerize.js
+++ jquery.centerize.js
@@ -3,8 +3,8 @@
 		var element = $(this);
 		element.css({
 			position: "absolute",
-			left: (($(window).width() - $(element).outerWidth()) / 2) + $(window).scrollTop() + "px",
-			top: (($(window).height() - $(element).outerHeight()) / 2) + $(window).scrollLeft() + "px"
+			left: (($(window).width() - $(element).outerWidth()) / 2) + $(window).scrollLeft() + "px",
+			top: (($(window).height() - $(element).outerHeight()) / 2) + $(window).scrollTop() + "px"
 		});
 		return element;
 	}
